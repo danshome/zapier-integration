56fe5b43c5e134f940b8410245ca050fc9a5fa33
tech-consortium <23320411+tech-consortium@users.noreply.github.com>
2023-11-14 19:46:38 -0600

    Changes to enforce code quality.

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es2021': true,
+    'node': true,
+    'mocha': true,
+  },
+  'extends': ['eslint:recommended', 'google'],
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
+    },
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+  },
+  'rules': {
+  },
+};
