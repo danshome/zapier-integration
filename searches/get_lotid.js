e744c21ddcef6da8786eb44de988320ab8ee414c
tech-consortium <23320411+tech-consortium@users.noreply.github.com>
2023-12-15 18:45:45 -0600

    Revert "Add request retry helper function"
    This reverts commit f7d72d46bb33e5667e054affc6b8bf4fe034d918.

--- searches/get_lotid.js
+++ searches/get_lotid.js
@@ -1,11 +1,26 @@
-const {requestWithRetry} = require('../lib/requestHelper');
-
 /**
  * The base URL for the wineries API.
  *
  * @type {string}
  */
 const BASE_URL = 'https://sutter.innovint.us/api/v1/wineries';
+
+/**
+ * Makes an asynchronous HTTP request using the provided parameters
+ *
+ * @param {object} z - The instance of the currently running Zapier app
+ * @param {string} url - The URL to which the request will be sent
+ * @param {string} method - The HTTP method to be used for the request, e.g. 'GET', 'POST', etc.
+ * @param {object} params - The parameters to be included with the request
+ * @param {object} headers - The headers to be included with the request
+ *
+ * @return {Promise<object>} - A Promise that resolves to the JSON data returned by the API
+ */
+async function request(z, url, method, params, headers) {
+  const response = await z.request(url, {method, params, headers});
+  response.throwForStatus();
+  return response.json();
+}
 
 /**
  * Retrieves lot data by case goods name.
@@ -25,7 +40,7 @@ const getLotByCaseGoodsName = async (z, bundle) => {
     'Authorization': `Access-Token ${bundle.authData.accessToken}`,
   };
   const url = `${BASE_URL}/${bundle.inputData.wineryId}/lots`;
-  const response = await requestWithRetry(z, url, 'GET',
+  const response = await request(z, url, 'GET',
       {limit: 1, q: bundle.inputData.caseGoodsName}, headers);
   const lotData = response.results[0]?.data;
   if (!lotData || !lotData.id) {
